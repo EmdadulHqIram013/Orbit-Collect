8540fff1d29fb7f6c0a9297834acc41102824e27
Wolf007 <99535270+EmdadulHqIram013@users.noreply.github.com>
2024-12-28 17:58:07 +1200

    settings.py

---
+++ config/settings.py
@@ -0,0 +1,11 @@
+SCREEN_WIDTH = 900
+SCREEN_HEIGHT = 700
+FRAME_RATE = 60
+COLORS = {
+    "background": (0.0, 0.0, 0.0),
+    "player": (1.0, 1.0, 1.0),
+    "maze": (0.0, 0.5, 0.8),
+    "obstacles": (1.0, 0.0, 0.0),
+    "orbs": (0.0, 1.0, 0.0),
+    "bullets": (1.0, 1.0, 0.0),
+}
